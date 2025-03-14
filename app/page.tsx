b8589e13f5bee173422a6af2f0c7fb41a9592c60
SATYAM_JOSHI <91068003+Satyam-joshi-a-Developer@users.noreply.github.com>
2025-03-14 23:06:50 +0530

    responsvie update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <div className="spline ">
-        <h1 className="absolute inset-0 flex items-center justify-center text-[30px] ">
-        I created this website to post my AI improvement from the start 
+        <h1 className="absolute inset-0 flex items-center justify-center text-[30px] mx-[30px]">
+        I created this website to post my AI improvement from the start.
         </h1>
 
         <div className="spline-object h-[100vh] bg-black">
